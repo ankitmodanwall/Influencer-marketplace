216c832b6fa6ddecceb621a82e98911ced6c20a0
Junaid <onlyjunaid2002@gmail.com>
2023-11-05 11:35:57 +0530

    remote

--- next.config.js
+++ next.config.js
@@ -2,13 +2,37 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: [
-      "storage.googleapis.com",
-      "t4.ftcdn.net",
-      "www.influencer.in",
-      "utfs.io",
-      "instagram.fdel45-1.fna.fbcdn.net",
-      "yt3.googleusercontent.com",
+    remotePatterns: [ 
+      {
+      protocol: 'https',
+      hostname: 'storage.googleapis.com',
+    },
+      {
+      protocol: 'https',
+      hostname: 't4.ftcdn.net',
+    },
+      {
+      protocol: 'https',
+      hostname: 'www.influencer.in',
+    },
+      {
+      protocol: 'https',
+      hostname: 'utfs.io',
+    },
+      {
+      protocol: 'https',
+      hostname: 'instagram.fdel45-1.fna.fbcdn.net',
+    },
+      {
+      protocol: 'https',
+      hostname: 'yt3.googleusercontent.com',
+    },
+      // "storage.googleapis.com",
+      // "t4.ftcdn.net",
+      // "www.influencer.in",
+      // "utfs.io",
+      // "instagram.fdel45-1.fna.fbcdn.net",
+      // "yt3.googleusercontent.com",
     ],
   },
 };
